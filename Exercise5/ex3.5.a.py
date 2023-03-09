a2040a083da5bc6424943b2d4d5cc34b41a826b6
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-09 00:15:27 -0700

    done Exercise 5

--- Exercise5/ex3.5.a.py
+++ Exercise5/ex3.5.a.py
@@ -4,3 +4,58 @@ Names: Hareem Khan, Eeman Abid
 Assignment: 3
 
 """
+
+#Task a: Search in a sorted Array
+
+
+
+import time
+import random
+import matplotlib.pyplot as plt
+
+#inneficient Implementation
+def search_sorted_array(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+#Efficient Implementation
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+def time_search_sorted_array(func, n):
+    arr = sorted(random.sample(range(1, n*10), n))
+    target = random.randint(1, n*10)
+    times = []
+    for i in range(100):
+        start_time = time.time()
+        func(arr, target)
+        end_time = time.time()
+        times.append(end_time - start_time)
+    return times
+
+n = 1000
+inefficient_times = time_search_sorted_array(search_sorted_array, n)
+efficient_times = time_search_sorted_array(binary_search, n)
+
+plt.hist(inefficient_times, alpha=0.5, label='Inefficient')
+plt.hist(efficient_times, alpha=0.5, label='Efficient')
+plt.legend(loc='upper right')
+plt.xlabel("Values")
+plt.ylabel("Measurements")
+plt.show()
+
+print("Inefficient average execution time: ", sum(inefficient_times) / len(inefficient_times))
+print("Efficient average execution time: ", sum(efficient_times) / len(efficient_times))
--- Exercise5/ex3.5.b.py
+++ Exercise5/ex3.5.b.py
@@ -4,3 +4,65 @@ Names: Hareem Khan, Eeman Abid
 Assignment: 3
 
 """
+#Task B:Insertion in and extraction from priority queue
+
+import random
+import time
+import statistics
+import matplotlib.pyplot as plt
+import heapq
+
+
+import random
+import time
+from heapq import heappop, heappush
+
+# Inefficient Implementation
+def inefficient_pqueue(num_elements):
+    pqueue = []
+    for i in range(num_elements):
+        heappush(pqueue, random.randint(0, 100))
+    for i in range(num_elements):
+        heappop(pqueue)
+
+# Efficient Implementation
+def efficient_pqueue(num_elements):
+    pqueue = []
+    for i in range(num_elements):
+        heappush(pqueue, random.randint(0, 100))
+    while pqueue:
+        heappop(pqueue)
+
+# Experiment Code
+num_measurements = 100
+num_elements = 1000
+
+inefficient_times = []
+efficient_times = []
+
+for i in range(num_measurements):
+    # Inefficient Implementation
+    start = time.time()
+    inefficient_pqueue(num_elements)
+    end = time.time()
+    inefficient_times.append(end-start)
+
+    # Efficient Implementation
+    start = time.time()
+    efficient_pqueue(num_elements)
+    end = time.time()
+    efficient_times.append(end-start)
+
+# Plot Distribution
+import matplotlib.pyplot as plt
+
+plt.hist(inefficient_times, bins=10, alpha=0.5, label='Inefficient')
+plt.hist(efficient_times, bins=10, alpha=0.5, label='Efficient')
+plt.legend(loc='upper right')
+plt.xlabel("Values")
+plt.ylabel("Measurements")
+plt.show()
+
+# Print Aggregate of Measured Values
+print(f'Inefficient: min={min(inefficient_times)}, avg={sum(inefficient_times)/num_measurements}')
+print(f'Efficient: min={min(efficient_times)}, avg={sum(efficient_times)/num_measurements}')
