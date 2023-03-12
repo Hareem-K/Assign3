cb22285cb68989eec49348811b226b499e81fe5e
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-11 22:42:54 -0700

    exercise 3 part 2

--- Exercise3/ex3.3.py
+++ Exercise3/ex3.3.py
@@ -13,4 +13,5 @@ for x in range(64):
     list.append(x)
     if sys.getsizeof(list) != old_capacity:
         new_capacity = sys.getsizeof(list)
-        print(f"Capacity changed from {old_capacity} to {new_capacity} bytes after adding {x+1} elements.")
+        print(f"The total capacity changed from {old_capacity} to {new_capacity} bytes after adding {x+1} elements. The new number of elements that can be stored is {new_capacity/8}")
+        old_capacity = new_capacity
